a833aed90862961bdf6d10c3a0085e59ffbed188
Isaac So <godzilladigi641@gmail.com>
2018-10-03 23:47:11 -0400

    taking names

--- Kate.py
+++ Kate.py
@@ -2,9 +2,22 @@ import util
 from random import randint
 
 greeting = ["hello","hi","howdy","sup","salutations"]
+yes = ["yes","yeah","yea","sure","yup","mhm"]
+name = None
 
 while True:
     u = input(": ")
     if u.lower() in greeting:
         util.delayPrint(greeting[randint(0,4)])
+        print("")
+        if name == None:
+            a = 0
+            while a == 0:
+                util.delayPrint("what's your name?")
+                name = input(": ")
+                util.delayPrint(name + "?")
+                yn = input(": ")
+                if yn.lower() in yes:
+                    util.delayPrint("nice to meet you, " name)
+                    a = 1
     print("")
