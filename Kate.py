6178a6cce4a035cae903a16dcf5988abdc906f79
Isaac So <godzilladigi641@gmail.com>
2018-10-04 00:30:55 -0400

    removing punct, request name if not given, splitting strings

--- Kate.py
+++ Kate.py
@@ -1,12 +1,14 @@
 import util
 from random import randint
 
-greeting = ["hello","hi","howdy","sup","salutations"]
+punct = "?!,.;()#$%^&*"
+greeting = ["hello","hi","howdy","sup","salutations","hello?"]
 yes = ["yes","yeah","yea","sure","yup","mhm"]
 name = None
 
 while True:
     u = input(": ")
+
     if u.lower() in greeting:
         util.delayPrint(greeting[randint(0,4)])
         print("")
@@ -18,6 +20,21 @@ while True:
                 util.delayPrint(name + "?")
                 yn = input(": ")
                 if yn.lower() in yes:
-                    util.delayPrint("nice to meet you, " name)
+                    util.delayPrint("nice to meet you " + name)
                     a = 1
-    print("")
+
+    if all(e in list(map(lambda b: b.strip(punct), u.split())) for e in ["my","name"]):
+        if name != None:
+            util.delayPrint(name + ", was it?")
+        else:
+            a = 0
+            while a == 0:
+                util.delayPrint("you never told me.")
+                util.pause(1)
+                util.delayPrint("what IS your name?")
+                name = input(": ")
+                util.delayPrint(name + "?")
+                yn = input(": ")
+                if yn.lower() in yes:
+                    util.delayPrint("nice to meet you " + name)
+                    a = 1
