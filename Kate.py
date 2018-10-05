bc763478b114899a16d99b4729a886595c6a75dc
Isaac So <godzilladigi641@gmail.com>
2018-10-04 23:54:41 -0400

    dear kate, today i taught you how to remember my name. i also taught you how to forget my name. and when you forget, just remember what i told you about remembering. you subconsciously do remember, cuz that's not how computers work. be all sarcastic when they try to trick you into giving you false info about their name. remember all these things in spirit and in truth for christ loved us first. in christ: isaac

--- Kate.py
+++ Kate.py
@@ -45,10 +45,67 @@ while True:
 
     if all(e in list(map(lambda b: b.strip(punct), u.split())) for e in ["my","name"]):
         if name != None:
-            util.delayPrint(name + ",")
-            util.pause(0.2)
-            util.delayPrint(" right?")
-            print("")
+            if randint(0,4) == 0:
+                util.delayPrint("i forgot")
+                util.pause(0.1)
+                util.delayPrint(".")
+                util.pause(0.1)
+                util.delayPrint(".")
+                util.pause(0.75)
+                util.delayPrint(" what was the first letter again?")
+                print("")
+
+                z = 1
+                while z == 1 or z == 5:
+                    if z == 5:
+                        util.pause(1)
+                        util.delayPrint("what was the first letter?")
+                        print("")
+                    fLetter = input(": ")
+                    if fLetter == name[0]:
+                        util.delayPrint("oh")
+                        util.pause(0.5)
+                        util.delayPrint(".")
+                        util.pause(0.5)
+                        util.delayPrint(".")
+                        util.pause(1)
+                        util.delayPrint(" OH YEA!")
+                        util.pause(0.25)
+                        util.delayPrint(" the second letter is \"" + name[1] + "\,")
+                        util.pause(0.15)
+                        util.delayPrint(" right???")
+                        print("")
+
+                        d = 1
+                        while d == 1 or d == 5:
+                            if d == 5:
+                                util.pause(1)
+                                util.delayPrint("is the second letter \"" + name[1] + "\"?")
+                                print("")
+                            yn = input(": ")
+                            if yn in yes:
+                                util.delayPrint(name + "!")
+                                util.pause(0.5)
+                                util.delayPrint(" your name is " + name + "! ^^")
+                                print("")
+                                z = 2
+                                d = 2
+                            else:
+                                util.delayPrint("who you gassing,")
+                                util.pause(0.15)
+                                util.delayPrint(" that's not how computers work")
+                                print("")
+                                d = 5
+                    else:
+                        util.delayPrint("nice try,")
+                        util.pause(1)
+                        print(" bub.")
+                        z = 5
+            else:
+                util.delayPrint(name + "!")
+                util.pause(1)
+                util.delayPrint(" well at least that's what you told me > . >")
+                print("")
         else:
             a = 0
             while a == 0:
